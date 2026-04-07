d5323eb5b3e14739ff60c27a0375120750228cc7
Souvik Dutta <souvikdutta5122005@gmail.com>
2026-04-07 19:46:56 +0530

    change path

--- backend/services/soil_type_classification/soil_loader.py
+++ backend/services/soil_type_classification/soil_loader.py
@@ -1,6 +1,6 @@
 import geopandas as gpd
 
-soil_gdf = gpd.read_file(r"C:\Users\basud\OneDrive\Documents\MERN PROJECTS\Final-Yr-Project\new ds\soil_map_ref\WB_SOIL_SHP.shp")
+soil_gdf = gpd.read_file(r"C:\Users\debas\OneDrive\Desktop\Final Year Project\Main_prj\new ds\soil_map_ref\WB_SOIL_SHP.shp")
 soil_gdf = soil_gdf.to_crs(epsg=4326)
 
 
