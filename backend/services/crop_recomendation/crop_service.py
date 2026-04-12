1420dbfc57d11b7f731d23f29f2c74e275e0e194
Souvik Dutta <souvikdutta5122005@gmail.com>
2026-04-12 21:51:35 +0530

    chceck 2.

--- backend/services/crop_recomendation/crop_service.py
+++ backend/services/crop_recomendation/crop_service.py
@@ -1,6 +1,51 @@
 import pandas as pd
 import joblib
 import numpy as np
+from backend.services.load_files import PREV_CROP_DATA_PATH
 
-df = pd.read
+df = pd.read_csv(PREV_CROP_DATA_PATH)
 
+model = joblib.load("model/crop_model.pkl")
+le = joblib.load("model/label_encoder.pkl")
+scaler = joblib.load("model/scaler.pkl")
+
+
+def get_crop_insights(state , district, features):
+    # Normalize text
+    df["state_name"] = df["state_name"].str.lower()
+    df["district_name"] = df["district_name"].str.lower()
+
+    state = state.lower()
+    district = district.lower()
+
+    # Current crops
+    filtered = df[
+        (df["state_name"] == state) &
+        (df["district_name"] == district)
+    ]
+
+    current_crops = list(filtered["crop_name"].unique()) if not filtered.empty else []
+
+    # ML Prediction (Top 3)
+    X = np.array([[
+        features["N"],
+        features["temperature"],
+        features["humidity"],
+        features["ph"],
+        features["rainfall"]
+    ]])
+
+    X_scaled = scaler.transform(X)
+
+    probs = model.predict_proba(X_scaled)
+    top3_idx = np.argsort(probs[0])[-3:]
+
+    predicted_crops = le.inverse_transform(top3_idx)
+
+    # Remove duplicates
+    extra_crops = [c for c in predicted_crops if c not in current_crops]
+
+    return {
+        "currentCrops": current_crops,
+        "recommendedCrops": extra_crops.tolist()
+    }
