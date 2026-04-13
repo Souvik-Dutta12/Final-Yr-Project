092b7db321326bdf194417339e66348fa77581bd
Subhadeep Pramanick <subhadeeppramanick10@gmail.com>
2026-04-13 09:10:03 +0530

    minorchange

--- backend/services/soil_type_classification/soil_loader.py
+++ backend/services/soil_type_classification/soil_loader.py
@@ -7,7 +7,7 @@ soil_gdf = soil_gdf.to_crs(epsg=4326)
 # from pathlib import Path
 # import geopandas as gpd
 
-# BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
-# soil_file = BASE_DIR / "new ds" / "soil_map_ref" / "WB_SOIL_SHP.shp"
+# BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# soil_file = BASE_DIR / "soil_map_ref" / "WB_SOIL_SHP.shp"
 
 # soil_gdf = gpd.read_file(soil_file)
