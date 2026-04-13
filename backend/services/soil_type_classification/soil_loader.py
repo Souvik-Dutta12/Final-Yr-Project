27be6850bdbcfe3d167d4112fc52c22325460ab6
Subhadeep Pramanick <subhadeeppramanick10@gmail.com>
2026-04-13 09:07:21 +0530

    minor change

--- backend/services/soil_type_classification/soil_loader.py
+++ backend/services/soil_type_classification/soil_loader.py
@@ -1,13 +1,13 @@
-# import geopandas as gpd
-
-# soil_gdf = gpd.read_file(r"C:\Users\basud\OneDrive\Documents\MERN PROJECTS\Final-Yr-Project\new ds\soil_map_ref\WB_SOIL_SHP.shp")
-# soil_gdf = soil_gdf.to_crs(epsg=4326)
-
-
-from pathlib import Path
 import geopandas as gpd
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
-soil_file = BASE_DIR / "new ds" / "soil_map_ref" / "WB_SOIL_SHP.shp"
+soil_gdf = gpd.read_file(r"C:\Users\subha\OneDrive\Desktop\FinalYearProject\subhadeep\Final-Yr-Project\new ds\soil_map_ref\WB_SOIL_SHP.shp")
+soil_gdf = soil_gdf.to_crs(epsg=4326)
 
-soil_gdf = gpd.read_file(soil_file)
+
+# from pathlib import Path
+# import geopandas as gpd
+
+# BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+# soil_file = BASE_DIR / "new ds" / "soil_map_ref" / "WB_SOIL_SHP.shp"
+
+# soil_gdf = gpd.read_file(soil_file)
