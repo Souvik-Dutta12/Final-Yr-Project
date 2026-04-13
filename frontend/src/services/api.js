d528d99164d6b94324c4b4b0b435872e87ea861d
Souvik Dutta <souvikdutta5122005@gmail.com>
2026-04-13 12:27:26 +0530

    check

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "http://localhost:8010/api/v1";
 
 export async function getSoilByPoint(lat, lon) {
   const res = await fetch(`${BASE_URL}/soil/point?lat=${lat}&lon=${lon}`);
