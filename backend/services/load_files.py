cde57d3e3533608980a2f27d78d0c8c1014990ff
Subham Mondal <subham.mondal.23@aot.edu.in>
2026-04-17 14:39:50 +0530

    added endpoint in router_notes.txt for crop recomendation

--- backend/services/load_files.py
+++ backend/services/load_files.py
@@ -1,6 +1,5 @@
 
 # soil-types
-<<<<<<< HEAD
 SOIL_TYPES = r"C:\Users\basud\OneDrive\Documents\MERN PROJECTS\Final-Yr-Project\new ds\soil_map_ref\WB_SOIL_SHP.shp"
 
 # Farmland Detection
@@ -14,18 +13,3 @@ N_PATH = r"C:\Users\basud\OneDrive\Documents\MERN PROJECTS\Final-Yr-Project\new 
 BULK_DENSITY_PATH = r"C:\Users\basud\OneDrive\Documents\MERN PROJECTS\Final-Yr-Project\new ds\quality\Bulk_Density.tif"
 CEC_PATH = r"C:\Users\basud\OneDrive\Documents\MERN PROJECTS\Final-Yr-Project\new ds\quality\CEC.tif"
 SOC_PATH = r"C:\Users\basud\OneDrive\Documents\MERN PROJECTS\Final-Yr-Project\new ds\quality\SOC.tif"
-=======
-SOIL_TYPES = r"C:\Users\subha\OneDrive\Desktop\FinalYearProject\my-frontend\new ds\soil_map_ref\WB_SOIL_SHP.shp"
-
-# Farmland Detection
-NDVI_PATH = r"D:\indices\indices\NDVI.tif"
-NDWI_PATH = r"D:\indices\indices\NDWI.tif"
-NDBI_PATH =  r"D:\indices\indices\NDBI.tif"
-
-# soil-quality
-PH_PATH = r"C:\Users\subha\OneDrive\Desktop\FinalYearProject\my-frontend\new ds\quality\pH.tif"
-N_PATH = r"C:\Users\subha\OneDrive\Desktop\FinalYearProject\my-frontend\new ds\quality\N.tif"
-BULK_DENSITY_PATH = r"C:\Users\subha\OneDrive\Desktop\FinalYearProject\my-frontend\new ds\quality\Bulk_Density.tif"
-CEC_PATH = r"C:\Users\subha\OneDrive\Desktop\FinalYearProject\my-frontend\new ds\quality\CEC.tif"
-SOC_PATH = r"C:\Users\subha\OneDrive\Desktop\FinalYearProject\my-frontend\new ds\quality\SOC.tif"
->>>>>>> 1931675b8ea328605bb3e7c460a9cea3dd46e04c
