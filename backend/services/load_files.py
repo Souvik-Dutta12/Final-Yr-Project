29421173193242fd1041465da9c54bc9e1f9bb3e
Souvik Dutta <souvikdutta5122005@gmail.com>
2026-04-20 15:44:28 +0530

    visualizze the changes

--- backend/services/load_files.py
+++ backend/services/load_files.py
@@ -2,11 +2,6 @@
 # soil-types
 SOIL_TYPES = r"C:\Users\debas\OneDrive\Desktop\Final Year Project\Main_prj\new ds\soil_map_ref\WB_SOIL_SHP.shp"
 
-# Farmland Detection
-NDVI_PATH = r"C:\Users\debas\OneDrive\Desktop\Final Year Project\Main_prj\new ds\WB\indices\NDVI.tif"
-NDWI_PATH = r"C:\Users\debas\OneDrive\Desktop\Final Year Project\Main_prj\new ds\WB\indices\NDWI.tif"
-NDBI_PATH =  r"C:\Users\debas\OneDrive\Desktop\Final Year Project\Main_prj\new ds\WB\indices\NDBI.tif"
-
 # soil-quality
 PH_PATH = r"C:\Users\debas\OneDrive\Desktop\Final Year Project\Main_prj\new ds\quality\pH.tif"
 N_PATH = r"C:\Users\debas\OneDrive\Desktop\Final Year Project\Main_prj\new ds\quality\N.tif"
